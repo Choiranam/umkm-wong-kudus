35ecd9d223bf14fa3e7448e3f43206404dc7a635
AzzanIsham <azzanishamkds@gmail.com>
2025-10-22 11:55:59 +0700

    Feat:[AI] Create Navbar & Footer

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,11 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TestColor from "../testing/TestColor";
+import HomePage from "../pages/HomePage";
 
 const AppRoutes = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<TestColor />} />
+                {/* Color Testing Route */}
+                <Route path="/color" element={<TestColor />} />
+
+                {/* Main Route */}
+                <Route path="/" element={<HomePage />} />
             </Routes>
         </Router>
     );
